3ce8182761ff5b50da4099ab551da031154fd5a7
cartermusee <cartermusee@gmail.com>
2024-01-04 19:13:26 +0300

    importing classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
