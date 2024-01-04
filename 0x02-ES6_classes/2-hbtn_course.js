53f15d27a7f547e9aa32f0125d316bd25b085e99
cartermusee <cartermusee@gmail.com>
2024-01-04 17:36:02 +0300

    setter and getter and validation in es6

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('Invalid input types');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+}
