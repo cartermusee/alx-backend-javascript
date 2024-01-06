3db2761a62e014649ec1e26bf2830f79fa08151f
cartermusee <cartermusee@gmail.com>
2024-01-06 22:50:23 +0300

    didd all remaining

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,7 +2,7 @@ export default function divideFunction(numerator, denominator) {
   let div;
   try {
     if (denominator === 0) {
-      throw new Error('cannot divide by 0');
+      throw Error('cannot divide by 0');
     }
     div = numerator / denominator;
   } catch (error) {
