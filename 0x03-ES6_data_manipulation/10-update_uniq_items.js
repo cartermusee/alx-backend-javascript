9a1886b592e854017fa934931839971423550895
cartermusee <cartermusee@gmail.com>
2024-01-12 17:17:36 +0300

    the extras

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,11 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof(Map))) {
+  if (!(map instanceof (Map))) {
     throw new Error('Cannot process');
   }
-  
   for (const [key, value] of map) {
     if (value === 1) {
-      map.set(key,100);
-    }
-    else {
+      map.set(key, 100);
+    } else {
       map.set(key, value);
     }
   }
