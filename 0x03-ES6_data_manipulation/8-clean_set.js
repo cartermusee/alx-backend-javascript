eef13b9cc696dfa4d4efb832490c4a21fff736ff
cartermusee <cartermusee@gmail.com>
2024-01-12 17:18:59 +0300

    the extras

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const string = [];
   set.forEach((value) => {
-    if (startString!== '' && value.startsWith(startString)) {
+    if (startString !== '' && value.startsWith(startString)) {
       string.push(value.substring(startString.length));
     }
   });
