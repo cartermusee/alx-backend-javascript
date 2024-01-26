11e53d25a8af51024b0b795bf7b7e91ff147866d
cartermusee <cartermusee@gmail.com>
2024-01-26 19:12:22 +0300

    multiple promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,18 @@
 import signUpuser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const sign = signUpuser(firstName, lastName);
   const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([sign, upload])
-    .then((result) => {
-      const values = [];
-      result.forEach((element) => {
-        if (element.status === 'fulfilled') {
-          values.push({ status: element.status, value: element.value });
-        } else {
-          values.push({ status: element.status, value: `${element.reason}` });
-        }
-      });
-      return result;
-    });
+  const result = await Promise.allSettled([sign, upload]);
+  const values_1 = [];
+  result.forEach((element) => {
+    if (element.status === 'fulfilled') {
+      values_1.push({ status: element.status, value: element.value });
+    } else {
+      values_1.push({ status: element.status, value: `${element.reason}` });
+    }
+  });
+  return result;
 }
