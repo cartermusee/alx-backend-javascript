04e9aff387777792a8230caec2657d3eeafb8b36
macilah-carter <museemacilah@gmail.com>
2024-02-27 12:43:54 +0300

    node js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+/**
+ * a small HTTP server using Express
+ */
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.listen(1245);
