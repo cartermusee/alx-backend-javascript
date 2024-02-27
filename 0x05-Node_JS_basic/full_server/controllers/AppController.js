835dcd1c5f3e06d8a3ec3087f524d37f51f497ba
macilah-carter <museemacilah@gmail.com>
2024-02-28 01:12:43 +0300

    full server

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * the Students controller
+ */
+
+class AppController{
+  static getHomepage(request, response){
+    response.status(200).send('Hello Holberton School!')
+  }
+}
+
+export default AppController;
---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,56 @@
+/**
+ * the Students controller
+ */
+import readDatabase from "../utils";
+
+class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const datapath = process.argv[2];
+      const data = await readDatabase(datapath);
+      const fields = Object.keys(data);
+      fields.sort((i, k) => {
+        if (i < k) {
+          return -1;
+        }
+        if (i > k) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+      response.statusCode = 200;
+      response,write('This is the list of our students\n');
+      for (const fd in fields) {
+        response.write(`Number of students in ${fd}: ${data[fd].length}. List: ${data[fd].join(', ')}`);
+        if (fields.indexOf(fd) !== fields.length - 1) response.write('\n');
+      }
+    } catch (err) {
+        response.statusCode = 500;
+      response.write(err.message);
+    }
+    response.end();
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+    if (['CS', 'SWE'].indexOf(major.toUpperCase()) === -1) {
+      response.statusCode = 500;
+      response.send('Major parameter must be CS or SWE');
+    } else {
+      const datapath = process.argv[2];
+      try {
+        const data = await readDatabase(datapath);
+        response.statusCode = 200;
+        response.write(`List: ${data[major.toUpperCase()].join(', ')}`);
+      } catch (err) {
+        response.statusCode = 500;
+        response.write(err.message);
+      }
+      response.end();
+    }
+  }
+
+};
+
+export default StudentsController
