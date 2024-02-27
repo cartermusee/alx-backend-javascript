ea32a31525f6b8dfe83d2946c37d55c18ffc3d83
macilah-carter <museemacilah@gmail.com>
2024-02-27 12:55:36 +0300

    node js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,48 @@
+/**
+ * a more complex HTTP server using Express
+ */
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      const values = data.split('\n');
+      const cs = [];
+      const swe = [];
+      values.forEach((value) => {
+        const field = value.split(',');
+        if (field.length > 3 && field !== null) {
+          if (field[3].trim() === 'CS') {
+            cs.push(field[0]);
+          } else if (field[3].trim() === 'SWE') {
+            swe.push(field[0]);
+          }
+        }
+      });
+      let str = `Number of students: ${cs.length + swe.length}\n`;
+      str += `Number of students in CS: ${cs.length}. List: ${cs.join(', ')}\n`;
+      str += `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`;
+      resolve(str);
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', async (req, res) => {
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n ${data}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(1245);
+module.exports = app;
