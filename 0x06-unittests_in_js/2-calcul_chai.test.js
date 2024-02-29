63fab5d4361b3f391b58c38734b3aaeca2375812
macilah-carter <museemacilah@gmail.com>
2024-02-29 21:21:54 +0300

    makechange

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,23 @@
 /**
  * tests for calculateNumber
  */
-const assert = require('chai').assert;
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function(){
   it('Returns the sum of two rounded nums', function(){
-    assert.equal(calculateNumber('SUM', 1, 3), 4);
-    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 
   it('Returns the subtraction of two rounded nums', function(){
-    assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
 
   it('Returns the division of two rounded nums', function(){
-    assert.equal(calculateNumber('DIVIDE', 4, 2), 2);
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
