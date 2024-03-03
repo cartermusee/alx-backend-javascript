deeb5be9cf8cd46848aa67dcd47167c0d3abcec1
macilah-carter <museemacilah@gmail.com>
2024-03-03 22:27:23 +0300

    test

---
+++ 0x06-unittests_in_js/-payment_token.test.js
@@ -0,0 +1,16 @@
+/**
+ * testing  getPaymentTokenFromAPI
+ */
+const assert = require('assert');
+const getPaymentTokenFromAPI = require('./6-payment_token.js');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('returns a promise', function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        assert.equal(res.data, 'Successful response from the API');
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
