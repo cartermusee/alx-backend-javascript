249d0ac81504f2e59ba59c5db7fb8f50e3709046
macilah-carter <museemacilah@gmail.com>
2024-03-03 23:03:44 +0300

    test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,7 +2,7 @@ const request = require('request');
 const { expect } = require('chai');
 
 
-describe('index page', function () {
+describe('cart page', function () {
   it('returns status code 200', function (done) {
     request.get('http://localhost:7865', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
@@ -12,7 +12,7 @@ describe('index page', function () {
   });
 
   it('returns correct result', function (done) {
-    request.get('http://localhost:7865/2', function (error, response, body) {
+    request.get('http://localhost:7865/cart/2', function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
@@ -20,14 +20,14 @@ describe('index page', function () {
   });
   
   it('returns 404 when no id', function (done) {
-    request.get('http://localhost:7865/lop', function (error, response, body) {
+    request.get('http://localhost:7865/cart/lop', function (error, response, body) {
       expect(response.statusCode).to.equal(404);
       done();
     });
   });
 
   it('returns 404 when wrong id', function (done) {
-    request.get('http://localhost:7865/no', function (error, response, body) {
+    request.get('http://localhost:7865/cart/no', function (error, response, body) {
       expect(response.statusCode).to.equal(404);
       done();
     });
